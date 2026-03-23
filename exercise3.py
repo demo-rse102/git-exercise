1d2dbf517e99713be3171c6a75357858bf41d2c8
Felix Neubauer <felix@neuby.de>
2026-03-23 17:12:42 +0100

    Extract interval check to own function

--- exercise3.py
+++ exercise3.py
@@ -32,8 +32,7 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= 0 and ll_px <= self._dx \
-            and ll_py >= 0 and ll_py <= self._dy
+        return is_in_interval(ll_px, 0.0, self._dx) and is_in_interval(ll_py, 0.0, self._dy) # Task A
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
@@ -41,7 +40,8 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
         
-    # def is_in_interval(...) -> bool: # Task A
+def is_in_interval(value: float, lower_bound: float, upper_bound: float) -> bool: # Task A
+    return lower_bound <= value <= upper_bound
 
 
 def test_rectangle_contains_exact() -> None:
